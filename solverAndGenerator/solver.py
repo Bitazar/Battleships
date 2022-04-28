288d4e8297925752a1a26fde496c071fdcc01859
Mateusz Jaracz <matjaracz@wp.pl>
2022-04-28 18:26:12 +0200

    changes in solver.py

--- solverAndGenerator/solver.py
+++ solverAndGenerator/solver.py
@@ -1,45 +1,46 @@
-from copy import deepcopy
+from abc import ABC, abstractmethod
+from typing import Any
 
 import numpy as np
+
+from constrains import Constrains
+from placer import PlacerInterface
 
 
 class Solver:
     def __init__(
             self,
-            value_set,
-            constrains,
-            placement,
-            finished,
-            depth=lambda x: x) -> None:
-        self.__value_set = value_set
-        self.__constrains = constrains
-        self.__placement = placement
-        self.__finished = finished
-        self.__solved_boards = []
-        self.__indexes = []
-        self.__depth = depth
+            constrains: list,
+            hard_constrains: Constrains,
+            soft_constrains: Constrains,
+            placer: PlacerInterface) -> None:
+        self.__constrains = {}
+        for index, states in constrains:
+            self.__constrains[index] = states
+        self.__hard_constrains = hard_constrains
+        self.__soft_constrains = soft_constrains
+        self.__placer = placer
 
-    def __generate_indexes(self, board: np.ndarray) -> None:
-        self.__indexes = []
-        for y, row in enumerate(board):
-            for x, cell in enumerate(row):
-                if not self.__depth(cell):
-                    self.__indexes.append((x, y))
+    @property
+    def constrains(self) -> map:
+        return self.__constrains
 
-    def __solve(self, board, step) -> None:
-        if step == len(self.__indexes):
-            if self.__finished(board):
-                self.__solved_boards.append(board)
-            return None
-        position = self.__indexes[step]
-        for value in self.__value_set:
-            temp_board = deepcopy(board)
-            if (self.__placement(temp_board, position, value) and
-                    self.__constrains(temp_board)):
-                self.__solve(temp_board, step + 1)
+    @property
+    def hard_constrains(self) -> Constrains:
+        return self.__hard_constrains
 
-    def __call__(self, board: np.ndarray) -> list:
-        self.__solved_boards = []
-        self.__generate_indexes(board)
-        self.__solve(board, 0)
-        return self.__solved_boards
+    @property
+    def soft_constrains(self) -> Constrains:
+        return self.__soft_constrains
+
+    @property
+    def placer(self) -> PlacerInterface:
+        return self.__placer
+
+    @abstractmethod
+    def __call__(
+            self,
+            width: int,
+            height: int,
+            init_vals: list) -> np.ndarray:
+        pass

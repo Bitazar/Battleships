010da4db1de5214e0afc18aaa944b29738c03f6b
Mateusz Jaracz <matjaracz@wp.pl>
2022-04-28 18:26:28 +0200

    changes in wave_function_collapse.py

--- solverAndGenerator/wave_function_collapse.py
+++ solverAndGenerator/wave_function_collapse.py
@@ -1,128 +1,137 @@
+from copy import deepcopy
 from itertools import chain
-from random import choice
+from random import shuffle, choice
 from math import inf
+from typing import Any
 
 import numpy as np
 
-WATER = (1, {
-    (0, 1): {1, 2, 3},
-    (0, -1): {1, 2, 3},
-    (-1, 0): {1, 2, 3},
-    (1, 0): {1, 2, 3},
-    (-1, 1): {1, 2, 3},
-    (1, 1): {1, 2, 3},
-    (-1, -1): {1, 2, 3},
-    (1, -1): {1, 2, 3}
-})
+from constrains import Constrains
+from placer import PlacerInterface
+from solver import Solver
+from states import States
 
-HOR_SHIP = (2, {
-    (0, 1): {1},
-    (0, -1): {1},
-    (-1, 0): {1, 2},
-    (1, 0): {1, 2},
-    (-1, 1): {1},
-    (1, 1): {1},
-    (-1, -1): {1},
-    (1, -1): {1}
-})
 
-VER_SHIP = (3, {
-    (0, 1): {1, 3},
-    (0, -1): {1, 3},
-    (-1, 0): {1},
-    (1, 0): {1},
-    (-1, 1): {1},
-    (1, 1): {1},
-    (-1, -1): {1},
-    (1, -1): {1}
-})
+class NoSolutionException(Exception):
+    def __init__(self) -> None:
+        super().__init__("No solution can be found for the given problem")
 
-# 1 - water
-# 2 - horizontal ship
-# 3 - vertical ship
 
-class WaveFunctionCollapse:
-    def __init__(self, constrains: list) -> None:
-        self.__constrains = {}
-        for index, states in constrains:
-            self.__constrains[index] = states
+class WaveFunctionCollapse(Solver):
+    def __init__(
+            self,
+            constrains: list,
+            hard_constrains: Constrains,
+            soft_constrains: Constrains,
+            placer: PlacerInterface,
+            states: States) -> None:
+        super().__init__(constrains, hard_constrains,
+            soft_constrains, placer)
+        self.__states = states
+
+    @property
+    def states(self) -> States:
+        return self.__states
 
     def __generate_board(self, width: int, height: int) -> np.ndarray:
-        init_superstate = {label for label in self.__constrains.keys()}
+        init_superstate = {label for label in self.constrains.keys()}
+        init_cell_value = self.placer.generate_cell(init_superstate)
         return np.array([
-            [init_superstate for x in range(width)]
+            [init_cell_value for x in range(width)]
             for y in range(height)
         ])
 
-    @staticmethod
-    def __min_entropy(board: np.ndarray) -> tuple:
-        lengths = np.array([[len(val) if len(val) > 1 else inf for val in row]
-            for row in board])
+    def __get_length(self, cell: Any) -> int:
+        length = len(self.placer.access_cell(cell))
+        return length if length > 1 else inf
+
+    def __min_entropy(self, board: np.ndarray) -> tuple:
+        lengths = np.array([[self.__get_length(cell)
+            for cell in row] for row in board])
         return np.argwhere(lengths == np.min(lengths))
 
-    @staticmethod
-    def __on_board(board: np.ndarray, position: tuple) -> bool:
-        ly, lx = board.shape
+    def __on_board(self, board: np.ndarray, position: tuple) -> bool:
+        lx = board.shape[0]
+        ly = board.shape[1]
         x, y = position
         return lx > x >= 0 and ly > y >= 0
 
-    @classmethod
-    def __get_neighourhood(cls, board: np.ndarray, position: tuple) -> bool:
+    def __get_neighourhood(
+            self,
+            board: np.ndarray,
+            position: tuple) -> bool:
         neighborhood = []
         x, y = position
         for u in [-1, 0, 1]:
             for v in [-1, 0, 1]:
                 if u or v:
                     neighborhood.append((x + u, y + v))
-        return [x for x in neighborhood if cls.__on_board(board, x)]
+        return [x for x in neighborhood if self.__on_board(board, x)]
 
-    def __available_states(
+    def __propagate(
             self,
             board: np.ndarray,
             position: tuple,
-            diff: tuple,
-            states: set) -> set:
-        x, y = position
-        n_states = states
-        for state in board[y][x]:
-            n_states = n_states.intersection(self.__constrains[state][diff])
-        return n_states
-
-    def __propagate(self, board: np.ndarray, position: tuple, checked) -> None:
+            checked: list) -> None:
         neighbours = self.__get_neighourhood(board, position)
         for nx, ny in neighbours:
             if (nx, ny) not in checked:
                 dx = nx - position[0]
                 dy = ny - position[1]
-                if len(board[ny][nx]) > 1:
-                    n_states = self.__available_states(board,
-                        position, (dx, dy), board[ny][nx])
-                    if n_states != board[ny][nx]:
-                        board[ny][nx] = n_states
+                if len(self.placer.access_cell(board[ny][nx])) > 1:
+                    n_states = self.states(self, board, position, (dx, dy))
+                    if n_states != self.placer.access_cell(board[ny][nx]):
+                        self.placer(board, (nx, ny), n_states)
                         checked.append((nx, ny))
                         self.__propagate(board, (nx, ny), checked)
 
-    def __collapse(self, board: np.ndarray) -> None:
-        y, x = choice(self.__min_entropy(board))
-        board[y][x] = {choice(list(board[y][x]))}
-        self.__propagate(board, (x, y), [(x, y)])
-
     def __is_collapsed(self, board: np.ndarray) -> bool:
-        return all(chain.from_iterable([[len(val) == 1 for val in row]
-            for row in board]))
+        return all(chain.from_iterable([[
+                len(self.placer.access_cell(cell)) <= 1 for cell in row
+            ] for row in board]))
 
-    @staticmethod
-    def __convert_to_collapsed(board: np.ndarray) -> np.ndarray:
+    def __check_constrains(self, board: np.ndarray) -> tuple:
+        if self.soft_constrains(board):
+            if self.__is_collapsed(board):
+                return board if self.hard_constrains(board) else None
+            elif (result := self.__collapse(board)) is not None:
+                return result
+        return None
+
+    def __collapse(self, board: np.ndarray) -> tuple:
+        y, x = choice(self.__min_entropy(board))
+        superposition = list(self.placer.access_cell(board[y][x]))
+        shuffle(superposition)
+        for state in superposition:
+            temp_board = deepcopy(board)
+            self.placer(temp_board, (x, y), {state})
+            self.__propagate(temp_board, (x, y), [(x, y)])
+            if (result := self.__check_constrains(temp_board)) is not None:
+                return result
+        return None
+
+    def __precollapse(self, board: np.ndarray, init_vals: list) -> None:
+        for x, y, state in init_vals:
+            self.placer(board, (x, y), {state})
+            self.__propagate(board, (x, y), [(x, y)])
+
+    def __unpack_state(self, cell) -> int:
+        val ,= self.placer.access_cell(cell)
+        return val
+
+    def __convert_to_collapsed(self, board: np.ndarray) -> np.ndarray:
         return np.array([
-            [val for val, in row]
+            [self.__unpack_state(cell) for cell in row]
             for row in board
         ])
 
-    def __call__(self, width: int, height: int) -> np.ndarray:
+    def __call__(
+            self,
+            width: int,
+            height: int,
+            init_vals: list) -> np.ndarray:
         board = self.__generate_board(width, height)
-        while not self.__is_collapsed(board):
-            self.__collapse(board)
-        return self.__convert_to_collapsed(board)
-
-wave = WaveFunctionCollapse([WATER, HOR_SHIP, VER_SHIP])
-print(wave(6, 6))
+        self.__precollapse(board, init_vals)
+        if (result := self.__collapse(board)) is not None:
+            return self.__convert_to_collapsed(result)
+        raise NoSolutionException()

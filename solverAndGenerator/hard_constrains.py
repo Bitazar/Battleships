b3ee9e08d18b43671a2d59a357c6b60a8a86286c
Mateusz Jaracz <matjaracz@wp.pl>
2022-04-28 18:25:52 +0200

    changes in hard_constrains.py

--- solverAndGenerator/hard_constrains.py
+++ solverAndGenerator/hard_constrains.py
@@ -1,57 +1,22 @@
 import numpy as np
 
+from placer import BattleshipsPlacer
+from constrains import Constrains
 
-class HardConstrains:
-    def __init__(self, constant_limits):
-        rl, cl, sl = constant_limits
-        self.__row_limits = rl
-        self.__column_limits = cl
-        self.__ship_len_limits = sl
 
-    @staticmethod
-    def __row_constrain(
+class HardConstrains(Constrains):
+    def __init__(
+            self,
+            constant_limits: list,
+            placer: BattleshipsPlacer):
+        super().__init__(constant_limits, placer)
+
+    def _row_constrain(
+            self,
             row: np.ndarray,
             row_sum: int) -> bool:
-        return len([val for val, _ in row if val > 1]) <= row_sum
+        ships, probs = self._contained(row)
+        return ships == row_sum and probs == 0
 
-    @staticmethod
-    def __column_constrain(
-            column: np.ndarray,
-            column_sum: int) -> bool:
-        return len([val for val, _ in column if val > 1]) <= column_sum
-
-    def board_constrain(self, board: np.ndarray) -> bool:
-        row_constrains = [self.__row_constrain(row, row_sum)
-            for row, row_sum in zip(board, self.__row_limits)]
-        column_constrains = [self.__column_constrain(board[:,i],
-            column_sum) for i, column_sum in enumerate(
-                self.__column_limits)]
-        return all(row_constrains) and all(column_constrains)
-
-    def __ship_no_more(self, ship_lengths: map) -> bool:
-        for key in sorted(self.__ship_len_limits.keys(), reverse=True):
-            if key not in ship_lengths.keys():
-                return True
-            if ship_lengths[key] != self.__ship_len_limits[key]:
-                return ship_lengths[key] < self.__ship_len_limits[key]
-        return True
-
-    def ship_length_constrain(self, board: np.ndarray):
-        ships = set()
-        for row in board:
-            for _, ship in row:
-                if ship:
-                    ships.add(tuple(ship))
-        ship_lengths = {}
-        for ship in ships:
-            if len(ship) not in ship_lengths.keys():
-                ship_lengths[len(ship)] = 1
-            else:
-                ship_lengths[len(ship)] += 1
-        return self.__ship_no_more(ship_lengths)
-
-    def __call__(self, board: np.ndarray) -> bool:
-        return (
-            self.board_constrain(board) and
-            self.ship_length_constrain(board)
-        )
+    def ship_length_constrain(self, board: np.ndarray) -> bool:
+        return self._get_ship_lengths(board) == self._ship_len_limits

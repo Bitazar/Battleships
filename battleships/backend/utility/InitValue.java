31f7deb26a3b4120fada48cbf68850a09b0fdaed
Mateusz Jaracz <matjaracz@wp.pl>
2022-05-22 14:37:28 +0200

    Added Init Value class

---
+++ battleships/backend/utility/InitValue.java
@@ -0,0 +1,42 @@
+package backend.utility;
+
+import java.util.Objects;
+
+public class InitValue {
+    private final Coord                 coord;
+    private final int                   value;
+
+    public InitValue(Coord coord, int value) {
+        this.coord = coord;
+        this.value = value;
+    }
+
+    public Coord getCoord() {
+        return coord;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof InitValue initValue)) return false;
+        return value == initValue.value && coord.equals(initValue.coord);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(coord, value);
+    }
+
+    @Override
+    public String toString() {
+        return "InitValue{" +
+                "coord=" + coord +
+                ", value=" + value +
+                '}';
+    }
+
+}
